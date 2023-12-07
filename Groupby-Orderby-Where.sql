14f0dd1644de97fc190c7c38cccc1dacab6769d4
adish-ct <adishathu525@gmail.com>
2023-12-07 11:06:07 +0530

    order by

--- Groupby-Orderby-Where.sql
+++ Groupby-Orderby-Where.sql
@@ -1,3 +1,5 @@
+----------------------------- GROUP BY ------------------------------------
+
 -- HAVING joins the GROUP BY party when you want to filter the results based on the result of aggregate functions. Here's how they play together:
 
 SELECT column1, aggregate_function(column2) as result
@@ -40,7 +42,69 @@ for row in result:
 
 
 
+---------------------------- WHERE ------------------------------
+
+-- The WHERE clause in SQL is used to filter records based on a specified condition. It's like a gatekeeper, 
+-- allowing only the rows that meet certain criteria to be included in the result set
+
+SELECT column1, column2, ...
+FROM table
+WHERE condition;
+
+-- example
+
+SELECT user_id, username, age
+FROM users
+WHERE age > 21;
+
+-- we can use logical and conditional operation also
+
+SELECT product_name, price
+FROM products
+WHERE category = 'Electronics' AND price < 1000;
+
+-- WHERE in Django Orm 
+
+from yourapp.models import User
+
+result = User.objects.filter(age__gt=21)
+
+for user in result:
+    print(user.username, user.age)
 
 
+-- various lookup types in Django, such as __exact, __iexact, __contains, __gt, __lt, __gte, __lte, etc.
+
+result = User.objects.filter(username__contains='john')
+
+
+----------------------------- ORDER BY ------------------------------------
+
+-- In SQL, the ORDER BY clause is used to sort the result set of a query based on one or more columns
+
+-- example
+
+SELECT column1, column2, ...
+FROM table
+ORDER BY column1 ASC, column2 DESC;
+
+SELECT product_name, price
+FROM products
+ORDER BY price DESC;
+
+
+-- ORDER BY in Django ORM
+
+from yourapp.models import Product
+
+result = Product.objects.all().order_by('price')
+
+for product in result:
+    print(product.product_name, product.price)
+
+result = Product.objects.all().order_by('category', '-price')
+
+-- order_by('price') sorts the result set based on the price column in ascending order. You can use 
+-- '-price' to sort in descending order.
 
 
